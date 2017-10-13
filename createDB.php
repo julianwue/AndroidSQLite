412778e0d45deb4c763ec0bed22da17efbf3bdea
Julian R <dreinetworks@gmail.com>
2017-10-13 17:35:24 +0200

    Comments with needed steps added

--- createDB.php
+++ createDB.php
@@ -1,1 +1,18 @@
 
+<?php
+
+/*
+Generate DB Classes in use of Android SQLiteOpenHelper. 
+This tool will generate one Table database
+
+Copyright JR 2017
+
+*/
+
+// Select Columns and Datatype
+
+// Click
+
+// Copy&Paste the parts to AndroidStudio
+
+?>
